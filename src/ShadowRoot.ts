30fea2a9135c35e7592ade20c5f35a3d89c4731f
zhanfeng.hzf <zhanfeng.hzf@alibaba-inc.com>
2019-07-24 20:08:14 +0800

    shadow-view@3.2.3

--- src/ShadowRoot.ts
+++ src/ShadowRoot.ts
@@ -5,8 +5,9 @@ export const supportShadow = "attachShadow" in document.createElement("div");
 
 export function attachShadow(host: Element, optinos: IShadowRootOptions) {
   if (!host || !supportShadow) return (host as any) as ShadowRoot;
-  const { mode = "open", delegatesFocus } = { ...optinos };
+  const { mode = "open", delegatesFocus = true } = { ...optinos };
   const shadowRoot = host.attachShadow({ mode, delegatesFocus });
+  (shadowRoot as any).style = {};
   bridgeShadowRoot(shadowRoot);
   return shadowRoot;
 }
