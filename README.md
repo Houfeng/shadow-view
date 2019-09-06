f98965b80aa3ed8e528cab88cfd3dc6a800199e9
zhanfeng.hzf <zhanfeng.hzf@alibaba-inc.com>
2019-09-06 13:10:45 +0800

    更新 README

--- README.md
+++ README.md
@@ -1,26 +1,50 @@
 # ShadowView
 
-一个针对 React 的 Shadow DOM 容器组件，利用 ShadowView 可更方便的在 React 应用中创建启用 Shadow DOM 的容器元素。
+ShadowView 是一个写好的可开箱即用的面向 React 的 Shadow DOM 容器组件，利用 ShadowView 可以像普通组件一样方便的在 React 应用中创建启用 Shadow DOM 的容器元素。
 
-# 安装
+ShadowView 目前完整兼容支持 React 15/16，组件的「事件处理、组件渲染更新」等行为在两个版中都是一致的。
+
+<a name="Dw1SA"></a>
+## 安装组件
 
 ```bash
 npm i shadow-view --save
 ```
 
-# 使用
+<a name="uwOxQ"></a>
+## 使用组件
 
-```tsx
+```javascript
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { ShadowView } from "shadow-view";
+
 function App() {
   return (
-    <ShadowView
-      scoped={{
-        style: `*{color:red;}`,
-        imports: ["aaa.css"]
-      }}
+    <ShadowView 
+    	styleContent={`*{color:red;}`} 
+			styleSheets={[
+    		'your_style1_url.css',
+      	'your_style2_url.css'
+    	]}
     >
+      <style>{`在这儿也可写内部样式`}</style>
       <div>这是一个测试</div>
     </ShadowView>
   );
 }
+
+ReactDOM.render(<App/>, document.getElementById('root'));
 ```
+
+<a name="ZMGXF"></a>
+## 组件属性
+
+| **属性名** | **类型** | **说明** |
+| :--- | :--- | :--- |
+| className | string | 组件自身 className |
+| style | any | 组件自身的内联样式 |
+| styleContent | string | 作用于 ShadowView 内部的样式 |
+| styleSheets | string[] | 作用于 ShadowView 内部的外联样式表 |
+| scoped | boolean | 是否开始隔离，默认为 true |
+| tagName | string | 外层容器 tagName，默认为 shadow-view |
--- src/example/index.tsx
+++ src/example/index.tsx
@@ -1,62 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { ShadowView, attachShadow } from "../";
+
+export class ShadowView extends React.Component {
+  attachShadow = (host: Element) => {
+    const shadowRoot = host.attachShadow({ mode: "open" });
+    [].slice.call(host.children).forEach((child: any) => {
+      shadowRoot.appendChild(child);
+    });
+  }
+  render() {
+    const { children } = this.props;
+    return <div ref={this.attachShadow}>
+      {children}
+    </div>;
+  }
+}
 
 export class App extends React.Component {
-  state = { msg: "" };
-
-  onClick = () => {
-    this.setState({ msg: "Hello" });
-  };
-
+  state = { message: '...' };
+  onBtnClick = () => {
+    this.setState({ message: 'haha' });
+  }
   render() {
-    const { msg } = this.state;
-    return (
-      <div>
-        <a href="/outside">Link outside</a>
-        <ShadowView>
-          {msg ? <div>{msg}</div> : <span>No message</span>}
-          <button onClick={this.onClick}>Click me</button>
-          <a href="/inside">Link inside</a>
-        </ShadowView>
-      </div>
-    );
-  }
-
-  async componentDidMount() {
-    await this.createPortal();
-  }
-
-  async componentWillMount() {
-    await this.removePortal();
-  }
-
-  portalRoot: HTMLElement;
-  portalStyle: HTMLElement;
-
-  async createPortal() {
-    const root = document.createElement("div");
-    document.body.appendChild(root);
-    const shadowRoot = attachShadow(root, { delegatesFocus: true });
-    const portalStyle = document.createElement("style");
-    portalStyle.setAttribute("scoped", "");
-    shadowRoot.appendChild(portalStyle);
-    const portalRoot = document.createElement("div");
-    shadowRoot.appendChild(portalRoot);
-    document.body.appendChild = (...args) => {
-      return portalRoot.appendChild(...args);
-    };
-    document.body.append = (...args) => {
-      return portalRoot.append(...args);
-    };
-    this.portalStyle = portalStyle;
-    this.portalRoot = portalRoot;
-  }
-
-  async removePortal() {
-    if (!this.portalRoot) return;
-    if (this.portalRoot.remove) return this.portalRoot.remove();
-    this.portalRoot.parentNode.removeChild(this.portalRoot);
+    const { message } = this.state;
+    return <div>
+      <ShadowView>
+        <div>{message}</div>
+        <button onClick={this.onBtnClick}>内部单击</button>
+      </ShadowView>
+      <button onClick={this.onBtnClick}>外部单击</button>
+    </div>
   }
 }
 
